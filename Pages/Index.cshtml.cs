5d390bf6c52523e6d82c099f0b2b2538f31e9413
Priscila Leite <109990443+PriciLeite@users.noreply.github.com>
2023-03-26 17:33:40 -0300

    Enviando projeto

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace MyRazorApp.Pages
+{
+    public class Index : PageModel
+    {       
+        public List<Category> Categories { get; set; } = new();
+
+        public async Task OnGet()
+        {
+            await Task.Delay(5000);
+            for(int i = 0; i <= 10; i++)
+            {
+                Categories.Add(
+                    item: new Category(
+                        i,
+                        Title:$"Categoria {i}",
+                        Price:i*18.95M));
+            }
+        }
+
+
+        public record Category(
+            int Id,
+            string Title,
+            decimal Price);
+        
+    
+    }
+}
